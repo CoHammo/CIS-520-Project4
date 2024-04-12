af174e0969ee9fbba9808c0f45074013248a5db4
Collin Hammond <cohammond12@gmail.com>
2024-04-12 13:59:31 -0500

    Updated pthread run script

--- 3way-pthread/run.sh
+++ 3way-pthread/run.sh
@@ -3,4 +3,6 @@
 # Script to easily run the program on Beocat. (Not Done)
 # Run the program with: build/scorecard-pthread [file] [number of threads]
 
-build/scorecard-pthread ../wiki_dump.txt 4
+#build/scorecard-pthread ../wiki_dump.txt 4
+
+sbatch --time=1 --mem-per-cpu=1G --cpus-per-task=1 --ntasks=1 --nodes=1 "build/scorecard-pthread ../wiki_dump.txt 4"
