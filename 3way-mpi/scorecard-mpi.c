4bc799273212b98b7615454a59bfdbebb5ac4560
Collin Hammond <cohammond12@gmail.com>
2024-04-18 14:29:00 -0500

    MPI sort of working

--- 3way-mpi/scorecard-mpi.c
+++ 3way-mpi/scorecard-mpi.c
@@ -21,17 +21,17 @@ typedef struct {
     int resultsSize; // initial allocation size of the results buffer
     char *buffer; // pointer to the buffer to read from
     char *results; // results buffer stores the highest ASCII value from each line the thread reads from
-    bool allocFail; // whether allocation of the results buffer failed for the thread
+    int allocFail; // whether allocation of the results buffer failed for the thread
 } thread_info_t;
 
 void *readLines(void *args) {
     thread_info_t *data = (thread_info_t *)args;
     data->results = calloc(data->resultsSize, sizeof(char)); // allocates results
     if (data->results == NULL) {
-        data->allocFail = true;
+        data->allocFail = 1;
         return NULL;
     }
-    data->allocFail = false;
+    data->allocFail = 0;
 
     // Seeks the correct start location
     if (data->index != 0) {
@@ -112,7 +112,6 @@ int main(int argc, char *argv[]) {
             perror(argv[1]);
             return -1;
         }
-        printf("Opened file\n");
 
         // Read the file into a buffer
         if (fread(mainBuffer, 1, stats.st_size, file) < 1) {
@@ -120,56 +119,76 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         fclose(file);
-        if (thread_info.buffer[stats.st_size-1] != '\n') thread_info.buffer[stats.st_size] = '\n';
-
-        printf("Read file\n");
+        if (mainBuffer[stats.st_size-1] != '\n') mainBuffer[stats.st_size] = '\n';
     }
 
-    // printf("About to get data\n");
-    // MPI_Barrier(MPI_COMM_WORLD);
-    // MPI_Bcast(mainBuffer, stats.st_size, MPI_CHAR, 0, MPI_COMM_WORLD);
-    // printf("Received Data");
-    // printf("%d: %s\n", thread_info.index, mainBuffer);
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast(mainBuffer, stats.st_size, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-    MPI_Finalize();
-
-/*
     thread_info.resultsSize = THREAD_RESULTS_START_SIZE;
     thread_info.linesCounted = 0;
     thread_info.start = thread_info.index * divide;
     if (thread_info.index < numThreads - 1) thread_info.end = divide + thread_info.start;
     else thread_info.end = thread_info.bufferLength;
+    thread_info.buffer = mainBuffer;
 
     readLines(&thread_info);
 
-    MPI_Finalize();
-
-    for (int i = 0; i < 1; i++) {
-        if (thread_info.allocFail) {
-            printf("Could not allocate thread %d results\n", i);
-            return 1;
-        }
-        
-        // Allocates results buffer based on lineCount
-        results = realloc(results, (lineCount + thread_info.linesCounted) * LINE_STRING_SIZE);
-        if (results == NULL) {
-            printf("Could not allocate results\n");
-            return 1;
-        }
-
-        // Gets the results from each thread and formats them into a string to eventually print
-        for (int j = 0; j < thread_info.linesCounted; j++) {
-            int index = (j + lineCount);
-            maxValuesIndex += snprintf(&(results[maxValuesIndex]), LINE_STRING_SIZE, "%d: %d\n", index, thread_info.results[j]);
-        }
-        lineCount += thread_info.linesCounted;
+    if (thread_info.index != 0) {
+        MPI_Send(&(thread_info.linesCounted), 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        printf("%d: Sent 0\n", thread_info.index);
+        MPI_Send(thread_info.results, thread_info.linesCounted, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        printf("%d: Sent 1\n", thread_info.index);
+        MPI_Send(&(thread_info.allocFail), 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        printf("%d: Sent 2\n", thread_info.index);
     }
 
-    printf("%s", results);
+    if (thread_info.index == 0) {
+        thread_info_t allData[numThreads];
+        for (int i = 1; i < numThreads; i++) {
+            int size = 0;
+            MPI_Recv(&size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            allData[i].results = calloc(size, sizeof(char));
+            MPI_Recv(allData[i].results, allData[i].linesCounted, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("%d: Received 1\n", thread_info.index);
+            MPI_Recv(&(allData[i].allocFail), 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("%d: Received 2\n", thread_info.index);
+        }
+        allData[0].results = calloc(thread_info.linesCounted, sizeof(char));
+        allData[0].results = thread_info.results;
+        allData[0].linesCounted = thread_info.linesCounted;
+        allData[0].allocFail = thread_info.allocFail;
 
-    free(thread_info.buffer);
+        for (int i = 0; i < numThreads; i++) {
+            if (allData[i].allocFail == 1) {
+                printf("Could not allocate thread %d results\n", i);
+                return 1;
+            }
+        
+            // Allocates results buffer based on lineCount
+            results = realloc(results, (lineCount + allData[i].linesCounted) * LINE_STRING_SIZE);
+            if (results == NULL) {
+                printf("Could not allocate results\n");
+                return 1;
+            }
+
+            // Gets the results from each thread and formats them into a string to eventually print
+            for (int j = 0; j < allData[i].linesCounted; j++) {
+                int index = (j + lineCount);
+                maxValuesIndex += snprintf(&(results[maxValuesIndex]), LINE_STRING_SIZE, "%d: %d\n", index, allData[i].results[j]);
+            }
+            lineCount += allData[i].linesCounted;
+        }
+
+        printf("%s", results);
+    }
+    
+
+    /*free(thread_info.buffer);
     free(results);
     for (int i = 0; i < numThreads; i++) {
         free(thread_info.results);
     }*/
+
+    MPI_Finalize();
 }
